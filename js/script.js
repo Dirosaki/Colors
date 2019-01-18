87174648b426729c599bf048fe29f9264bd4a8b4
Dirosaki <diegosgomes27@gmail.com>
2019-01-18 16:37:21 -0200

    conclusão parte 3 de 3

--- js/script.js
+++ js/script.js
@@ -16,6 +16,12 @@ let middle = document.querySelector('.middle');
 let loading = document.querySelector('.screen-loading');
 let cubos = document.querySelectorAll('.cubo');
 
+let body2 = document.querySelector('.body-two');
+let contain2 = document.querySelector('.contain-two');
+let colorHex = document.querySelectorAll('.colorHex');
+let refresh = document.querySelector('.refresh');
+let divisions = document.querySelectorAll('.division');
+
 // gera uma cor aleatória em hexadecimal
 function gera_cor1(){
     let hexadecimais = '0123456789ABCDEF';
@@ -79,12 +85,25 @@ setInterval(exitLoading, 3250);
 // deixa o input pronto para digitação
 input1.focus();
 
+let div1 = document.querySelector('.div1');
+let div2 = document.querySelector('.div2');
+let div3 = document.querySelector('.div3');
+let div4 = document.querySelector('.div4');
+
 // função de clique para descer enter color e subir color random
 menu.onclick = () => {
     menu.classList.toggle('fa-cog-click');
     contain.classList.toggle('contain-toggle');
+    contain2.classList.toggle('contain-two-toggle');
+    body2.classList.toggle('body-two-toggle');
+    div1.classList.toggle('div-toggle');
+    div2.classList.toggle('div-toggle');
+    div3.classList.toggle('div-toggle');
+    div4.classList.toggle('div-toggle');
     
 }
+
+
 
 // função que verifica o campo do input, se os requisitos forem aprovados ele aplica a cor digitada
 function apply () {
@@ -158,6 +177,226 @@ input2.onclick = () => {
     setTimeout(limpar, 1000);
 }
 
+let square = document.querySelectorAll('.bg-bubbles li');
+
+
+function colorGenerator(){
+    // Cubo animate color one
+    let opacity0 = Math.floor(Math.random() * 99);
+    let squareHex0 = '0123456789ABCDEF';
+    let squareColor0 = '#';
+  
+    for (let i = 0; i < 6; i++ ) {
+        squareColor0 += squareHex0[Math.floor(Math.random() * 16)];
+    }
+
+    square[0].style.opacity = `0.${opacity0}`;
+    square[0].style.backgroundColor = squareColor0;
+
+    // Cubo animate color two
+    let opacity1 = Math.floor(Math.random() * 99);
+    let squareHex1 = '0123456789ABCDEF';
+    let squareColor1 = '#';
+  
+    for (let i = 0; i < 6; i++ ) {
+        squareColor1 += squareHex1[Math.floor(Math.random() * 16)];
+    }
+
+    square[1].style.opacity = `0.${opacity1}`;
+    square[1].style.backgroundColor = squareColor1;
+
+    // Cubo animate color tree
+    let opacity2 = Math.floor(Math.random() * 99);
+    let squareHex2 = '0123456789ABCDEF';
+    let squareColor2 = '#';
+  
+    for (let i = 0; i < 6; i++ ) {
+        squareColor2 += squareHex2[Math.floor(Math.random() * 16)];
+    }
+
+    square[2].style.opacity = `0.${opacity2}`;
+    square[2].style.backgroundColor = squareColor2;
+
+    // Cubo animate color four
+    let opacity3 = Math.floor(Math.random() * 99);
+    let squareHex3 = '0123456789ABCDEF';
+    let squareColor3 = '#';
+  
+    for (let i = 0; i < 6; i++ ) {
+        squareColor3 += squareHex3[Math.floor(Math.random() * 16)];
+    }
+
+    square[3].style.opacity = `0.${opacity3}`;
+    square[3].style.backgroundColor = squareColor3;
+
+    // Cubo animate color five
+    let opacity4 = Math.floor(Math.random() * 99);
+    let squareHex4 = '0123456789ABCDEF';
+    let squareColor4 = '#';
+  
+    for (let i = 0; i < 6; i++ ) {
+        squareColor4 += squareHex4[Math.floor(Math.random() * 16)];
+    }
+
+    square[4].style.opacity = `0.${opacity4}`;
+    square[4].style.backgroundColor = squareColor4;
+
+    // Cubo animate color six
+    let opacity5 = Math.floor(Math.random() * 99);
+    let squareHex5 = '0123456789ABCDEF';
+    let squareColor5 = '#';
+  
+    for (let i = 0; i < 6; i++ ) {
+        squareColor5 += squareHex5[Math.floor(Math.random() * 16)];
+    }
+
+    square[5].style.opacity = `0.${opacity5}`;
+    square[5].style.backgroundColor = squareColor5;
+
+    // Cubo animate color seven
+    let opacity6 = Math.floor(Math.random() * 99);
+    let squareHex6 = '0123456789ABCDEF';
+    let squareColor6 = '#';
+  
+    for (let i = 0; i < 6; i++ ) {
+        squareColor6 += squareHex6[Math.floor(Math.random() * 16)];
+    }
+
+    square[6].style.opacity = `0.${opacity6}`;
+    square[6].style.backgroundColor = squareColor6;
+
+    // Cubo animate color eight
+    let opacity7 = Math.floor(Math.random() * 99);
+    let squareHex7 = '0123456789ABCDEF';
+    let squareColor7 = '#';
+  
+    for (let i = 0; i < 6; i++ ) {
+        squareColor7 += squareHex7[Math.floor(Math.random() * 16)];
+    }
+
+    square[7].style.opacity = `0.${opacity7}`;
+    square[7].style.backgroundColor = squareColor7;
+
+    // Cubo animate color nine
+    let opacity8 = Math.floor(Math.random() * 99);
+    let squareHex8 = '0123456789ABCDEF';
+    let squareColor8 = '#';
+  
+    for (let i = 0; i < 6; i++ ) {
+        squareColor8 += squareHex8[Math.floor(Math.random() * 16)];
+    }
+
+    square[8].style.opacity = `0.${opacity8}`;
+    square[8].style.backgroundColor = squareColor8;
+
+    // Cubo animate color ten
+    let opacity9 = Math.floor(Math.random() * 99);
+    let squareHex9 = '0123456789ABCDEF';
+    let squareColor9 = '#';
+  
+    for (let i = 0; i < 6; i++ ) {
+        squareColor9 += squareHex9[Math.floor(Math.random() * 16)];
+    }
+
+    square[9].style.opacity = `0.${opacity9}`;
+    square[9].style.backgroundColor = squareColor9;
+
+    return squareColor0, squareColor1, squareColor2, squareColor3, squareColor4, squareColor5, squareColor6, squareColor7, squareColor8, squareColor9;
+}
+
+colorGenerator();
+
+// let time = setTimeout(colorGenerator, 500);
+
+
+let degs = 90;
+
+function randomColor(){
+    // Primeira cor
+    let hexadecimais1 = '0123456789ABCDEF';
+    let color1 = '#';
+  
+    // Pega um número aleatório no array acima
+    for (let i = 0; i < 6; i++ ) {
+    //E concatena à variável cor
+        color1 += hexadecimais1[Math.floor(Math.random() * 16)];
+    }
+    divisions[0].style.background = color1;
+    colorHex[0].value = color1;
+    
+    // Segunda cor
+    let hexadecimais2 = '0123456789ABCDEF';
+    let color2 = '#';
+    
+    for (let i = 0; i < 6; i++ ) {
+        color2 += hexadecimais2[Math.floor(Math.random() * 16)];
+    }
+    divisions[1].style.background = color2;
+    colorHex[1].value = color2
+
+    // Terceira cor
+    let hexadecimais3 = '0123456789ABCDEF';
+    let color3 = '#';
+  
+    for (let i = 0; i < 6; i++ ) {
+        color3 += hexadecimais3[Math.floor(Math.random() * 16)];
+    }
+
+    divisions[2].style.background = color3;
+    colorHex[2].value = color3;
+
+    // Quarta cor
+    let hexadecimais4 = '0123456789ABCDEF';
+    let color4 = '#';
+  
+    for (let i = 0; i < 6; i++ ) {
+        color4 += hexadecimais4[Math.floor(Math.random() * 16)];
+    }
+
+    divisions[3].style.background = color4;
+    colorHex[3].value = color4;
+    return color1, color2, color3, color4;
+}
+
+randomColor();
+
+refresh.onclick = () => {
+    degs += 360;
+    refresh.style.transform = `rotate(${degs}deg)`; 
+    randomColor();
+    colorGenerator();
+}
+
+colorHex[0].onclick = () => {
+    colorHex[0].select();
+    document.execCommand('copy');
+    // colorHex[0].setSelectionRange(0, 0);
+   
+    // setTimeout(limpar, 1000);
+}
+colorHex[1].onclick = () => {
+    colorHex[1].select();
+    document.execCommand('copy');
+    colorHex[1].setSelectionRange(0, 0);
+   
+    // setTimeout(limpar, 1000);
+}
+colorHex[2].onclick = () => {
+    colorHex[2].select();
+    document.execCommand('copy');
+    colorHex[2].setSelectionRange(0, 0);
+   
+    // setTimeout(limpar, 1000);
+}
+colorHex[3].onclick = () => {
+    colorHex[3].select();
+    document.execCommand('copy');
+    colorHex[3].setSelectionRange(0, 0);
+   
+    // setTimeout(limpar, 1000);
+}
+
+
 // função que converte código hexadecimal para RGB
 function hexToRgb(hex) {
     let shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
@@ -188,11 +427,4 @@ function girar(){
     input2.value = result;
     
 };
-
-
-
-
-
-
-
 
